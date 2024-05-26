14227ed4df7eee84b69ab9721903ade922cad965
jiyoung park <asa855@nate.com>
2024-05-26 22:34:17 +0900

    [feat] 로컬 maria db 연결

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from '../i18n'
 import Calendar from '../ui/calendar/Calendar'
+import pool from '../lib/db'
 type Props = {
     params: {
         lng: any
@@ -8,5 +9,16 @@ type Props = {
 
 export default async function Home({ params: { lng } }: Props) {
     const { t } = await useTranslation(lng)
+
+    const testConnection = async () => {
+        try {
+            const [rows, fields] = await pool.query('SELECT * from tb_event')
+            console.log('The solution is: ', rows)
+        } catch (error) {
+            console.error('Unable to connect to the database:', error)
+        }
+    }
+
+    testConnection()
     return <Calendar />
 }
